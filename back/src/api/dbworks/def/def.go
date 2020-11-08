77f2737652a46e66b63223a86c5c14420a4a53e5
tocchy-tocchy <tochigi-reina@g.ecc.u-tokyo.ac.jp>
2020-11-09 00:17:58 +0900

    階層分けて管理しやすくした

---
+++ back/src/api/dbworks/def/def.go
@@ -0,0 +1,12 @@
+package def
+
+const (
+	DBAdmin   = "mysql"
+	User      = "tocchy"
+	Pass      = "*******"
+	Protocol  = "tcp(127.0.0.1:3306)"
+	Database  = "daily_todo"
+	Char      = "utf8mb4"
+	ParseTime = "True"
+	Loc       = "Local"
+)
---
+++ back/src/api/dbworks/todo.go
@@ -0,0 +1,15 @@
+package dbworks
+
+import (
+	"./model"
+
+	"gorm.io/gorm"
+)
+
+func GetAll(db *gorm.DB) []model.ToDoList {
+
+	var todo []model.ToDoList
+	db.Find(&todo)
+
+	return todo
+}
